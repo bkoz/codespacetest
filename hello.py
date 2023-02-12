3bbdb57770cd20db7de80dbae1b5f1942ce5c5c0
Bob Kozdemba <bkozdemba@gmail.com>
2023-02-12 18:19:19 +0000

    updated

--- hello.py
+++ hello.py
@@ -5,10 +5,14 @@ A simple example
 import numpy as np
 import datetime
 import time
+from datetime import timezone
 
 secs = time.time()
-dt = datetime.datetime
-print(f'Today is {secs}')
+dt_today = datetime.datetime.today()
+print(f'Today is {dt_today}')
+dt_now = datetime.datetime.now()
+print(f'Today is {dt_now}')
+
 a = np.array([1,2])
 b = np.array([3,4])
 
